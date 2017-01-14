bcb774d9e3fafdc8feadb0dd1125f120704ed3c0
Michael Okarimia <git@michaelokarimia.com>
2017-01-14 12:35:20 +0000

    Calculator returns requested amount

--- test/acceptance/test_input.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-    # pylint: disable=R0904
-class TestInput(unittest.TestCase):
-
-    def test_returnsrequestedloanamount(self):
-        loan = 1000
-        self.assertEqual(loan, 1000)
--- test/unit/test_calculator.py
+++ test/unit/test_calculator.py
@@ -3,10 +3,12 @@ import lib.calculator as calculator
 
 # pylint: disable=R0904
 class TestLoanCalculator(unittest.TestCase):
+    requestedamount = 1000
 
     def setUp(self):
+
         self.calculator = calculator.Calculator()
 
-    def test_returns_a_decision(self):
-        mydecision = self.calculator.getdecision()
-        self.assertEqual(mydecision.requestedamount, 1000)
+    def test_returns_requested_loan(self):
+        mydecision = self.calculator.getdecision(self.requestedamount)
+        self.assertEqual(mydecision.requestedamount, self.requestedamount)
