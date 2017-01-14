919d85c9a03de1afd68ae95707b2c30cc9ef7159
Michael Okarimia <git@michaelokarimia.com>
2017-01-14 23:17:34 +0000

    Lender data loaded from file

--- app/lenders.py
+++ app/lenders.py
@@ -5,13 +5,17 @@ class Lenders(object):
     def __init__(self, filename):
         self.filename = filename
         self.lenderscount = 0
+        self.lenders = dict()
 
     def load_lenders(self):
         marketfile = io.open(self.filename, encoding='utf-8')
         marketfile.readline() #assumes first line is columns names in csv file
 
         for line in marketfile:
-            print line
             self.lenderscount += 1
+            mylist = line.split(',')
+            lendic = {mylist[0]:[float(mylist[1]), [float(mylist[2])]]}
+            print lendic
+            self.lenders.update(lendic)
 
         marketfile.close()
--- tests/unit/test_lenders.py
+++ tests/unit/test_lenders.py
@@ -8,3 +8,6 @@ class TestLender(unittest.TestCase):
         lenderlist = lenders.Lenders(self.filename)
         lenderlist.load_lenders()
         self.assertEqual(lenderlist.lenderscount, 7)
+        self.assertEqual(len(lenderlist.lenders), 7)
+        self.assertEqual(lenderlist.lenders['Bob'][0], float(0.075))
+        self.assertEqual(lenderlist.lenders['Bob'][1][0], float(640.0))
