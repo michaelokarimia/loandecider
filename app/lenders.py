58e959ef383f1bed114df1b30b98f383d20eb0df
Michael Okarimia <git@michaelokarimia.com>
2017-01-15 12:12:01 +0000

    Safely open file

--- app/lenders.py
+++ app/lenders.py
@@ -1,5 +1,4 @@
-import io
-
+import codecs
 class Lenders(object):
 
     def __init__(self, filename):
@@ -8,14 +7,15 @@ class Lenders(object):
         self.lenders = dict()
 
     def load_lenders(self):
-        marketfile = io.open(self.filename, encoding='utf-8')
-        marketfile.readline() #assumes first line is columns names in csv file
 
-        for line in marketfile:
-            self.lenderscount += 1
-            mylist = line.split(',')
-            lendic = {mylist[0]:[float(mylist[1]), [float(mylist[2])]]}
-            print lendic
-            self.lenders.update(lendic)
+        with codecs.open(self.filename, 'r', encoding='utf-8') as marketfile:
+            marketfile.readline() #assumes first line is columns names in csv file
+
+            for line in marketfile:
+                self.lenderscount += 1
+                mylist = line.split(',')
+                lendic = {mylist[0]:[float(mylist[1]), [float(mylist[2])]]}
+                print lendic
+                self.lenders.update(lendic)
 
         marketfile.close()
