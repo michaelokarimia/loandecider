bc78c89067ac7eb6d26e6fd70b2a0154efb39362
Michael Okarimia <git@michaelokarimia.com>
2017-01-16 12:34:19 +0000

    Lender fetches most compettive rate
    From all the offers available, pick the best rate

--- app/lenders.py
+++ app/lenders.py
@@ -40,24 +40,19 @@ class Lenders(object):
             runningtotal += value
         return runningtotal
 
-    def get_decision(self, adecision):
-        if self.is_sufficient_offers(adecision.requestedamount):
-            adecision.rate = 0.075
-        return adecision
+    def get_decision(self, decision):
+        if self.is_sufficient_offers(decision.requestedamount):
+            decision.rate = self.get_best_rate_from_lenders(decision.requestedamount)
+        return decision
 
     def get_best_rate_from_lenders(self, loan):
 
         offers = {key:value for key, value in self.lenders.items()}
-        print "offers {0}".format(offers)
 
         bestrate = 1.0
         for dummy_key, value in offers.items():
-            print "value is {0}".format(value)
-            print "value loan is {0}".format(value[1])
-            print "value rate is {0}".format(value[0])
 
             if value[1] >= loan and value[0] < bestrate:
                 bestrate = value[0]
 
-        print bestrate
         return bestrate
